d09afd654fa6a26b58aff5e01a39eb5e8c616e34
YunByungil <dbsquddlfz@naver.com>
2022-11-16 20:17:38 +0900

    2022-11-16 no10989 카운팅 정렬 이해 잘 안 됨

---
+++ 정렬/bronze/no10989/Main.java
@@ -0,0 +1,26 @@
+package 정렬.bronze.no10989;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] check = new int[10001];
+        for (int i = 0; i < n; i++) {
+            check[Integer.parseInt(br.readLine())]++;
+        }
+
+        for(int i = 1; i < 10001; i++) {
+            while (check[i] > 0) {
+                sb.append(i).append('\n');
+                check[i]--;
+            }
+        }
+        System.out.println(sb);
+    }
+}
