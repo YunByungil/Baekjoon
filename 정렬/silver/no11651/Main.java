6d802fd46575f72ceb6c5b247f0117109aea62c0
YunByungil <dbsquddlfz@naver.com>
2022-11-18 14:27:35 +0900

    2022-11-18 정렬 9번 Comparator 활용 완료

--- 정렬/silver/no11651/Main.java
+++ 정렬/silver/no11651/Main.java
@@ -1,4 +1,4 @@
-package 정렬.silver.no11650;
+package 정렬.silver.no11651;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,24 +11,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
         int[][] test = new int[n][2];
+
         for (int i = 0; i < test.length; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             test[i][0] = Integer.parseInt(st.nextToken());
             test[i][1] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 0; i < test.length; i++) {
-            for (int j = 0; j < test[i].length; j++) {
-                System.out.println("test["+i+"]["+j+"] = " + test[i][j]);
-            }
-        }
+
         Arrays.sort(test, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                if (o1[0] == o2[0]) {
-                    return o1[1] - o2[1];
-                } else {
+                if (o1[1] == o2[1]) {
                     return o1[0] - o2[0];
+                } else {
+                    return o1[1] - o2[1];
                 }
             }
         });
