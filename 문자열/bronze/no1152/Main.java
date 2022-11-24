ae10b53839738371e599ca0522c1ebc70401af80
YunByungil <dbsquddlfz@naver.com>
2022-11-24 18:33:36 +0900

    문자열: no1152 / 6번 문제 StringTokenizer를 이용한 방법 (구글)

--- 문자열/bronze/no1152/Main.java
+++ 문자열/bronze/no1152/Main.java
@@ -3,20 +3,14 @@ package 문자열.bronze.no1152;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String word = br.readLine();
-        int count = 1;
-        for (int i = 1; i < word.length() - 1; i++) {
-            if (word.charAt(i) == ' ') {
-                count++;
-            }
-        }
-        if (word.equals(" ")) {
-            count = 0;
-        }
-        System.out.println(count);
+        StringTokenizer st = new StringTokenizer(word, " ");
+        System.out.println(st.countTokens());
     }
 }
