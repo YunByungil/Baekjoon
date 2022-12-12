a13fca7185c8272900bff642c2307c2367e9b797
YunByungil <dbsquddlfz@naver.com>
2022-12-12 14:59:01 +0900

    브루트포스: no1463 / 5번 문제: 영화감독 숌 / 풀긴했으나 효율적이지 못한 방법임

---
+++ 브루트포스/silver/no1463/Main.java
@@ -0,0 +1,28 @@
+package 브루트포스.silver.no1463;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            String str = "" + i;
+            if (str.contains("666")) {
+                list.add(i);
+            }
+            if(list.size() == 10000) {
+                break;
+            }
+        }
+
+        System.out.println(list.get(n - 1));
+    }
+}
