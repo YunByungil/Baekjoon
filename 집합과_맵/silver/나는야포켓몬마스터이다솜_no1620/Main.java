5260a7ca1e5b91b1e0ad90e8b50e1fc73249939e
YunByungil <dbsquddlfz@naver.com>
2022-12-12 22:52:08 +0900

    집합과 맵: no1620 / 3번 문제: 나는야 포켓몬 마스터 이다솜 / 완료

---
+++ 집합과_맵/silver/나는야포켓몬마스터이다솜_no1620/Main.java
@@ -0,0 +1,34 @@
+package 집합과_맵.silver.나는야포켓몬마스터이다솜_no1620;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Map<String, String> dictionary = new LinkedHashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            dictionary.put(name, String.valueOf(i));
+            dictionary.put(String.valueOf(i), name);
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            String find = st.nextToken();
+            sb.append(dictionary.get(find)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
