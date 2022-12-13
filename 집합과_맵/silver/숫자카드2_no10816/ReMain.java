6f72a1dd8df5605ebe1e372b1bdf2a35a6da9955
YunByungil <dbsquddlfz@naver.com>
2022-12-13 17:07:29 +0900

    집합과 맵: no10816 / 4번 문제: 숫자카드2 / 중복값이 있을 때 이진 탐색 완벽 이해x

---
+++ 집합과_맵/silver/숫자카드2_no10816/ReMain.java
@@ -0,0 +1,74 @@
+package 집합과_맵.silver.숫자카드2_no10816;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class ReMain {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < m; i++) {
+            int key = Integer.parseInt(st.nextToken());
+
+            sb.append(upperBound(arr, key) - low(arr, key)).append(" ");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    public static int low(int[] arr, int key) {
+        int lo = 0;
+        int hi = arr.length;
+
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+
+            if (key <= arr[mid]) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+
+    static int upperBound(int[] arr, int key) {
+        int lo = 0;
+        int hi = arr.length;
+
+        // lo가 hi랑 같아질 때 까지 반복
+        while (lo < hi) {
+
+            int mid = (lo + hi) / 2; // 중간위치를 구한다.
+
+            // key값이 중간 위치의 값보다 작을 경우
+            if (key < arr[mid]) {
+                hi = mid;
+            }
+            // 중복원소의 경우 else에서 처리된다.
+            else {
+                lo = mid + 1;
+            }
+
+        }
+
+        return lo;
+    }
+}
