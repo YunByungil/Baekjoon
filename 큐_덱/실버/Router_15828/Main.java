d355371a7c6e2136b6da8160246e2f1e5ed78d19
YunByungil <dbsquddlfz@naver.com>
2022-12-17 16:10:31 +0900

    큐, 덱: no15828 / 3번 문제: Router / 해결 못함 다시 풀어보기

---
+++ 큐_덱/실버/Router_15828/Main.java
@@ -0,0 +1,21 @@
+package 큐_덱.실버.Router_15828;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 1. input 받기 n => 라우터에 존재하는 버퍼의 크기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        // 2. -1을 입력하면 종료
+        int result = 0;
+
+        // 3. 둘째 줄부터 라우터가 처리해야 될 정보 입력
+        while (result != -1) {
+
+        }
+    }
+}
