664ede00b6b7f192f9f59d6742f53f625bc6847e
YunByungil <dbsquddlfz@naver.com>
2022-12-28 16:16:54 +0900

    투 포인터: no1644 / 4번 문제: 소수의 연속합 / 에라토스테네스의 체 알고리즘 학습 후 해결 완료

--- 투포인터/골드/소수의연속합_1644/Main.java
+++ 투포인터/골드/소수의연속합_1644/Main.java
@@ -11,31 +11,35 @@ public class Main {
         // 1. input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-//
-//        int[] arr = new int[n];
-//        for (int i = 0; i < n; i++) {
-//            arr[i] = i + 1;
-//        } // 1부터 n까지 숫자를 배열에 담고, 소수 찾기
 
-        List<Integer> list = new ArrayList<>();
-        int count = 0;
+        int[] arr = new int[n + 1];
+
         for (int i = 2; i <= n; i++) {
-            for (int j = 2; j <= i; j++) {
-                if (i % j == 0) {
-                    count++;
-                }
-            }
-            if (count == 1) {
-                list.add(i);
-            }
-            count = 0;
+            arr[i] = i;
         }
 
         // 2. 소수인지 아닌지 판별 ( 1은 소수가 아니다 )
         // 어떻게 판별할 수 있을까..?
         // 2로 나누었을 때, 나머지 1, 그럼 15같은 수는 어떻게 해결?
-        // 3. 소수가 아니라면 부분합을 이용해서 더함
-        count = getNum(list, n);
+        // 에라토스테네스의 체 알고리즘을 이용하자
+        for (int i = 2; i <= n; i++) {
+            if (arr[i] == 0) {
+                continue;
+            }
+            for (int j = i + i; j <= n; j+=i) {
+                arr[j] = 0;
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i : arr) {
+            if (i != 0) {
+                list.add(i);
+            }
+        }
+
+
+        // 3. 소수가 아니라면 부분합을 이용해서 더함 (getNum 메서드)
+        int count = getNum(list, n);
         System.out.println(count);
 
     }
@@ -59,5 +63,4 @@ public class Main {
         }
         return count;
     }
-
 }
