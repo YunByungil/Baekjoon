2c0126849fa1e7060be050d5732e129b3f3e6d10
YunByungil <dbsquddlfz@naver.com>
2022-12-28 11:34:59 +0900

    투 포인터: no1644 / 4번 문제: 소수의 연속합 / 시간 초과로 실패

---
+++ 투포인터/골드/소수의연속합_1644/Main.java
@@ -0,0 +1,63 @@
+package 투포인터.골드.소수의연속합_1644;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 1. input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+//
+//        int[] arr = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = i + 1;
+//        } // 1부터 n까지 숫자를 배열에 담고, 소수 찾기
+
+        List<Integer> list = new ArrayList<>();
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                list.add(i);
+            }
+            count = 0;
+        }
+
+        // 2. 소수인지 아닌지 판별 ( 1은 소수가 아니다 )
+        // 어떻게 판별할 수 있을까..?
+        // 2로 나누었을 때, 나머지 1, 그럼 15같은 수는 어떻게 해결?
+        // 3. 소수가 아니라면 부분합을 이용해서 더함
+        count = getNum(list, n);
+        System.out.println(count);
+
+    }
+
+    public static int getNum(List<Integer> list, int n) {
+        int start = 0; // 시작
+        int end = 0; // 끝
+        int sum = 0;
+        int count = 0;
+        while (true) {
+            if (sum >= n) {
+                if (sum == n) {
+                    count++;
+                }
+                sum -= list.get(start++);
+            } else if (end == list.size()) {
+                break;
+            } else if (sum < n) {
+                sum += list.get(end++);
+            }
+        }
+        return count;
+    }
+
+}
