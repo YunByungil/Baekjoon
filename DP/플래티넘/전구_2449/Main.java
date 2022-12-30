a5a142f39c21b271c5893e4b03524a79327790c1
YunByungil <dbsquddlfz@naver.com>
2022-12-30 13:50:11 +0900

    DP: no2449 / 1번 문제: 전구 / 해설 보고 1차 완료 ( 다시 풀기)

--- DP/플래티넘/전구_2449/Main.java
+++ DP/플래티넘/전구_2449/Main.java
@@ -17,58 +17,135 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken()); // 전구의 개수
         int k = Integer.parseInt(st.nextToken()); // 전구가 표현할 수 있는 색의 개수
-        arr = new int[n]; // 전구들 배열
+        arr = new int[n + 1]; // 전구들 배열
+        int[][] dp = new int[n + 1][n + 1];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        // 1. 입력 받으면서 중복된 숫자를 제거
+        for (int i = 1; i <= n; i++) { // 입력 받으면서 중복된 숫자를 제거
             arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for (int i = 0; i < n; i++) {
-            dfs(n, i, 1, 2);
-        }
-//        dfs(n, 0, 2, 3);
-
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        System.out.println("count : " + count);
-
-
-    }
-    // change : 내가 바꾸려고 하는 수
-    // check : 내가 바꾸려고 하는 index
-    // k : 원래 그 index의 값
-
-
-    public static void dfs(int n, int check, int k, int change) {
-        if (map.containsKey(check)) {
-            if (map.get(check) == change) {
-                return;
+            if (arr[i] == arr[i - 1]) {
+                n--;
+                i--;
             }
         }
-        map.put(check, change);
-        System.out.println("check : " + check);
-        // 탈출 조건
-        if (check < 0 || check == n) {
-            return;
+
+        // 2. dp 배열 초기화
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+            }
+            dp[i][i] = 0;
         }
 
-        if (arr[check] == k) {
-            count++;
-            arr[check] = change;
-        } else {
-//            count--;
-            return;
+        // 3. 전구 최솟값 구하기
+        /*
+        [1, 1] [2, 2] [3, 3] [4, 4] [5, 5]
+        0 0 0 0 0
+         */
+
+        /*
+        [1, 2] [2, 3] [3, 4] [4, 5]
+        - [1, 1] + [2, 2] + a[1] vs a[2]
+        0 + 0 + 1
+        - [2, 2] + [3, 3] + a[2] vs a[3]
+        0 + 0 + 1
+        - [3, 3] + [4, 4] + a[3] vs a[4]
+        0 + 0 + 1
+        - [4, 4] + [5, 5] + a[4] vs a[5]
+        0 + 0 + 1
+         */
+
+        // start : 1 ~ 4
+        // mid : 1 ~ 4, mid + 1 : 2 ~ 5
+        // end : 2 ~ 5 => size == 2일 때 2, size == 3일 때, 3
+        // end => size가 2일 때 1개 더 큼, size가 3일 때 2개 더 큼
+        // end = start + size - 1
+        // 1 + 2, 2 + 2, 3 + 2, 4 + 2,
+
+        /*
+        [1, 2] [2, 3] [3, 4] [4, 5]
+        1 1 1 1
+         */
+        /*
+        [1, 3] [2, 4] [3, 5]
+        - [1, 1] + [2, 3] + a[1] vs a[2]
+        0 + 1 + 1
+        - [1, 2] + [3, 3] + a[1] vs a[3]
+        1 + 0 + 1
+        - [2, 2] + [3, 4] + a[2] vs a[3]
+        0 + 1 + 1
+        - [2, 3] + [4, 4] + a[2] vs a[4]
+        1 + 0 + 0
+        - [3, 3] + [4, 5] + a[3] vs a[4]
+        0 + 1 + 1
+        - [3, 4] + [5, 5] + a[3] vs a[5]
+        1 + 0 + 1
+         */
+
+        /*
+        [1, 3] [2, 4] [3, 5]
+        2 1 2
+         */
+
+        /*
+        [1, 4] [2, 5]
+        - [1, 1] + [2, 4] + a[1] vs a[2]
+        0 + 1 + 1
+        - [1, 2] + [3, 4] + a[1] vs a[3]
+        1 + 1 + 1
+        - [1, 3] + [4, 4] + a[1] vs a[4]
+        2 + 0 + 1
+
+        - [2, 2] + [3, 5] + a[2] vs a[3]
+        0 + 2 + 1
+        - [2, 3] + [4, 5] + a[2] vs a[4]
+        1 + 1 + 0
+        - [2, 4] + [5, 5] + a[2] vs a[5]
+        1 + 0 + 1
+         */
+
+        /*
+        [1, 4] [2, 5]
+        2 2
+         */
+
+        /*
+        [1, 5] == 최종값
+        [1, 1] + [2, 5] + a[1] vs a[2]
+        [1, 2] + [3, 5] + a[1] vs a[3]
+        [1, 3] + [4, 5] + a[1] vs a[4]
+        [1, 4] + [5, 5] + a[1] vs a[5]
+         */
+        /*
+        size = 2
+        start = 1 ~ 4
+        end = 2, 3, 4, 5
+        mid = 1~1, 1~2, 1~3, 1~4
+         */
+        for (int size = 2; size <= n; size++) {
+            for (int start = 1; start <= n - size + 1; start++) {
+                int end = size + start - 1;
+                for (int mid = start; mid < end; mid++) {
+                    int newValue = dp[start][mid] + dp[mid + 1][end];
+                    if (arr[start] != arr[mid + 1]) {
+                        newValue++;
+                    }
+                    if (dp[start][end] > newValue) {
+                        dp[start][end] = newValue;
+                    }
+                }
+            }
         }
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
 
-        dfs(n, check - 1, k, change);
-        dfs(n, check + 1, k, change);
-    }
-
-    public static void changeNum() {
+        System.out.println("result : " + dp[1][n]);
 
     }
 }
