d3a69a93ea8632191f669bf72c745439f112fa8e
YunByungil <dbsquddlfz@naver.com>
2023-01-03 14:55:57 +0900

    DP: no2449 / 1번 문제: 전구 / 설명 추가

--- DP/플래티넘/전구_2449/ReMain.java
+++ DP/플래티넘/전구_2449/ReMain.java
@@ -19,14 +19,14 @@ public class ReMain {
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
             a[i] = Integer.parseInt(st.nextToken());
-            if (a[i] == a[i - 1]) {
+            if (a[i] == a[i - 1]) { // 중복된 숫자를 빼는 과정
                 i--;
                 n--;
             }
         }
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+            for (int j = 1; j <= n; j++) { // [1, 1] [2, 2] [3, 3]....은 어차피 경우의 수가 0이다.
                 dp[i][j] = Integer.MAX_VALUE;
             }
             dp[i][i] = 0;
@@ -34,10 +34,10 @@ public class ReMain {
 
         // 식을 이용해서 dp의 값을 채워넣으면 된다.
 
-        for (int size = 2; size <= n; size++) {
-            for (int start = 1; start <= n - size + 1; start++) {
-                int end = size + start - 1;
-                for (int mid = start; mid < end; mid++) {
+        for (int size = 2; size <= n; size++) { // 2부터 시작하는 이유는 [1, 2] [2, 3].... 을 구해야 되기 때문
+            for (int start = 1; start <= n - size + 1; start++) { // [1, 2] [2, 3] [3, 4] [4, 5] -> [1, 3] [2, 4] [3, 5].... 값을 채워넣기
+                int end = size + start - 1; // [1, 2]의 값을 구할 때, [2, 3]의 값을 구할 때 end 값 (2, 3)
+                for (int mid = start; mid < end; mid++) { // [1, 2]를 구할 때, [1, 1] + [2, 2]를 한다. 따라서 미드는 start, end 사이
                     int newValue = dp[start][mid] + dp[mid + 1][end];
                     if (a[start] != a[mid + 1]) {
                         newValue++;
@@ -50,11 +50,6 @@ public class ReMain {
             }
         }
 
-
-
-
         System.out.println("dp[1][5] = " + dp[1][n]);
-
-
     }
 }
