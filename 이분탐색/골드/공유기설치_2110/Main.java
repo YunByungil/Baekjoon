a99edf4e086db056faa99bd414c173a120d0681a
YunByungil <dbsquddlfz@naver.com>
2023-01-10 14:05:14 +0900

    이분 탐색: no2110 / 5번 문제: 공유기 설치 / 오답 완료, 생각이 너무 틀에 박혀있다. 더 연습하자

--- 이분탐색/골드/공유기설치_2110/Main.java
+++ 이분탐색/골드/공유기설치_2110/Main.java
@@ -12,6 +12,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        /*
+        생각이 너무 틀에박혀있다.
+        응용력이 너무 부족,
+        사실 이 문제는 그동안 풀어왔던 방식이랑 다를바 없는 문제다.
+
+        5집에서 3개의 공유기를 설치하는데
+        첫번째 집은 무조건 설치해야 됨. 그냥 당연한 거임
+        그럼 1~9의 집 중 9 - 1 / 2는 4잖아?
+        최대 거리가 4가 될 수 있는지 공유기를 다 설치해보는 거야.
+        안 되면 다시 4에서 반을 줄인 상태로 설치해보는 거고.
+        그동안 해왔던 방식 upperBound로 풀면 됨
+
+         */
 
         int n = Integer.parseInt(st.nextToken()); // 집의 개수 n
         int c = Integer.parseInt(st.nextToken()); // 공유기의 개수 c
