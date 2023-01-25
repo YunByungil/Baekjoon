6b17edd5006d986f33e2ce0e8a74a19e4ee809a6
YunByungil <dbsquddlfz@naver.com>
2023-01-25 22:23:23 +0900

    우선순위 큐: no1927 / 2번 문제: 최소 힙 / 최대 최소 PriorityQueue 매개변수 확인 완료

---
+++ 우선순위큐/실버/최소힙_1927/Main.java
@@ -0,0 +1,33 @@
+package 우선순위큐.실버.최소힙_1927;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (pq.isEmpty() && x == 0) {
+                sb.append(x).append("\n");
+            }
+
+            if (!pq.isEmpty() && x == 0) {
+                sb.append(pq.poll()).append("\n");
+            }
+
+            if (x != 0) {
+                pq.add(x);
+            }
+        }
+        System.out.println(sb);
+    }
+}
