2d18715b0a0fe0580275e8e675d9fd75aab495b8
YunByungil <dbsquddlfz@naver.com>
2023-02-13 10:49:56 +0900

    그래프와 순회: no2178 / 9번 문제: 미로 탐색 / 오답 완료

--- 그래프와순회/실버/미로탐색_2178/Main.java
+++ 그래프와순회/실버/미로탐색_2178/Main.java
@@ -12,13 +12,14 @@ public class Main {
     public static int count = 1;
     public static int n, m; // 도착 위치 (n, m)
 
-    public static int[] newX = {0, 0, 1, -1};
-    public static int[] newY = {1, -1, 0, 0};
+    public static int[] col = {0, 0, 1, -1};
+    public static int[] row = {1, -1, 0, 0};
 
     public static boolean[][] visit;
     public static int[][] arr; // 미로를 담는 2차원 배열
 
-    public static Queue<Integer> q = new LinkedList<>();
+    public static Queue<spot> q = new LinkedList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -35,51 +36,52 @@ public class Main {
                 arr[i][j + 1] = location.charAt(j) - '0';
             }
         }
-//        q.offer(1);
-//        bfs();
-        dfs();
-        System.out.println("count = " + count);
-    }
-
-    public static void dfs() {
+        visit[1][1] = true;
+        bfs(1, 1);
+        System.out.println("arr[n][m] = " + arr[n][m]);
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (!visit[i][j] && arr[i][j] == 1) {
-                    check(i, j);
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void bfs(int x, int y) {
+        q.add(new spot(x, y));
+
+        while (!q.isEmpty()) {
+            spot s = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int newX = s.x + col[i];
+                int newY = s.y + row[i];
+                if (newX > m || newY > n) {
+                    continue;
                 }
+
+                if (visit[newY][newX] || arr[newY][newX] == 0) {
+                    continue;
+                }
+                System.out.println("newX = " + newX);
+                System.out.println("newY = " + newY);
+                System.out.println("=");
+
+                q.add(new spot(newX, newY));
+                visit[newY][newX] = true;
+                arr[newY][newX] = arr[s.y][s.x] + 1;
             }
         }
     }
 
-    public static void check(int y, int x) {
-        if (y == n && x == m) {
-            finish = -1;
-        }
-        visit[y][x] = true;
+    public static class spot {
+        int x;
+        int y;
 
-        for (int i = 0; i < 4; i++) {
-            int xv = x + newX[i];
-            int yv = y + newY[i];
-
-            if (xv < m + 1 && yv < n + 1) {
-                if (!visit[yv][xv] && arr[yv][xv] == 1) {
-                    System.out.println("yv = " + yv);
-                    System.out.println("xv = " + xv);
-                    count++;
-                    check(yv, xv);
-                }
-            }
+        public spot(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 
-//    public static void bfs() {
-//        visit[1][1] = true;
-//
-//        while (!q.isEmpty()) {
-//            int newValue = q.poll();
-//            for (int i = 1; i <= n; i++) {
-//
-//            }
-//        }
-//    }
 }
