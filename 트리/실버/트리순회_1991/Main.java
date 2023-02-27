34a2ab82a66b9302b1e5a4820ff98d9be8da5fbf
YunByungil <dbsquddlfz@naver.com>
2023-02-27 13:54:02 +0900

    트리: no1991 / 4번 문제: 트리 순회 / 오답 완료

--- 트리/실버/트리순회_1991/Main.java
+++ 트리/실버/트리순회_1991/Main.java
@@ -4,82 +4,72 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static StringBuilder sb = new StringBuilder();
-    public static List<List<Character>> list = new ArrayList<>();
-    public static boolean[] visit;
-    public static char[] arr;
+    public static int[][] arr;
+    public static List<List<Integer>> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        arr = new char[n + 1];
-        visit = new boolean[n + 1];
-        for (int i = 0; i <= n; i++) {
-            list.add(new ArrayList<>());
-        }
-
-        for (int i = 1; i < n; i++) {
+        list.add(new ArrayList<>());
+        arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            char skip = st.nextToken().charAt(0);
-            int num = skip - '0' - 16;
-            char a = st.nextToken().charAt(0);
-            char b = st.nextToken().charAt(0);
-//                list.get(i).add(a);
-//                list.get(i).add(b);
-                list.get(num).add(a);
-                list.get(num).add(b);
-//            if (a != ('.')) {
-//                list.get(num).add(a);
-//            }
-//            if (b != ('.')) {
-//                list.get(num).add(b);
-//            }
-        }
-        System.out.println("list = " + list);
-        Arrays.fill(arr, ' ');
-//        dfs(1);
-        sb.append("\n");
-        dfs2(1);
-//        sb.append("\n");
-//        dfs3(1);
+            int num = st.nextToken().charAt(0) - 'A';
+            int a = st.nextToken().charAt(0) - 'A';
+            int b = st.nextToken().charAt(0) - 'A';
 
-        System.out.println("sb = " + sb);
+            if (a == -19) {
+                arr[num][0] = -1;
+            } else {
+                arr[num][0] = a;
+            }
+
+            if (b == -19) {
+                arr[num][1] = -1;
+            } else {
+                arr[num][1] = b;
+            }
+        }
+
+        preOrder(0);
+        System.out.println();
+        inOrder(0);
+        System.out.println();
+        postOrder(0);
+
+
 
     }
 
-    public static void dfs(int start) {
-        sb.append((char)(start + 'A' - 1));
-
-        for (char newValue : list.get(start)) {
-            int value = newValue - '0' - 16;
-            dfs(value);
-        }
-    }
-
-    public static void dfs2(int start) {
-        if (list.get(start).get(0) == '.') {
+    public static void preOrder(int start) {
+        if (start == -1) {
             return;
         }
-        dfs2(list.get(start).get(0) - 'A' + 1);
-        sb.append((char)(start + 'A' + 1));
-        dfs2(list.get(start).get(1) - 'A' + 1);
-        return;
 
+        System.out.print((char)(start + 'A'));
+        preOrder(arr[start][0]);
+        preOrder(arr[start][1]);
     }
-
-    public static void dfs3(int start) {
-        for (char newValue : list.get(start)) {
-            int value = newValue - '0' - 16;
-            dfs3(value);
-            System.out.println("value = " + value);
+    public static void inOrder(int start) {
+        if (start == -1) {
+            return;
         }
-//        sb.append(newValue);
-        sb.append((char)(start + 'A' - 1));
+
+        inOrder(arr[start][0]);
+        System.out.print((char)(start + 'A'));
+        inOrder(arr[start][1]);
+    }
+    public static void postOrder(int start) {
+        if (start == -1) {
+            return;
+        }
+
+        postOrder(arr[start][0]);
+        postOrder(arr[start][1]);
+        System.out.print((char)(start + 'A'));
     }
 }
