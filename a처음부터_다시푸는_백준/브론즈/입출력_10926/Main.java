e31bfa9e303b617345a5c0f1c581265909a75a8f
YunByungil <dbsquddlfz@naver.com>
2023-03-23 00:15:13 +0900

    백준: no10926 / 문제: ??! / 해결 완료

---
+++ a처음부터_다시푸는_백준/브론즈/입출력_10926/Main.java
@@ -0,0 +1,14 @@
+package a처음부터_다시푸는_백준.브론즈.입출력_10926;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        System.out.println(str+"??!");
+    }
+}
