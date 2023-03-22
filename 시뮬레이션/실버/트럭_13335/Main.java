53349c8984386306798fd9af8a63d792ceac91c5
YunByungil <dbsquddlfz@naver.com>
2023-03-22 15:41:44 +0900

    시뮬레이션: no13335 / 문제: 트럭 / 틀렸음, 다시 풀기

---
+++ 시뮬레이션/실버/트럭_13335/Main.java
@@ -0,0 +1,53 @@
+package 시뮬레이션.실버.트럭_13335;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n, w, l;
+    public static int count, check, sum;
+    public static int[] arr2;
+    public static Queue<Integer> q = new LinkedList<>();
+    public static Queue<Integer> arr = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        w = Integer.parseInt(st.nextToken());
+        l = Integer.parseInt(st.nextToken());
+
+        arr2 = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+           arr.add(Integer.parseInt(st.nextToken()));
+        }
+
+        for (int i = 0; i < w; i++) {
+            q.add(0);
+        }
+
+        while (!q.isEmpty()) {
+            count++;
+
+            check -= q.poll();
+
+            if (!arr.isEmpty()) {
+                if (arr.peek() + check <= l) {
+                    check += arr.peek();
+                    q.offer(arr.poll());
+                } else {
+                    q.offer(0);
+                }
+            }
+        }
+
+        System.out.println("count = " + count);
+
+    }
+}
