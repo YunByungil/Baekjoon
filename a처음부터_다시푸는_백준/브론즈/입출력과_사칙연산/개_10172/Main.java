ade239ac441024b9bb77575c003e4fb0e7e481e0
YunByungil <dbsquddlfz@naver.com>
2023-03-31 19:13:44 +0900

    백준: no10172 / 문제: 개 / 입출력과 사칙연산 파트 끝

---
+++ a처음부터_다시푸는_백준/브론즈/입출력과_사칙연산/개_10172/Main.java
@@ -0,0 +1,11 @@
+package a처음부터_다시푸는_백준.브론즈.입출력과_사칙연산.개_10172;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
