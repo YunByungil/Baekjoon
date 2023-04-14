83b78dc072b16d5f4cb84f3110b7e7f705135af1
YunByungil <dbsquddlfz@naver.com>
2023-04-14 21:03:10 +0900

    백준: no1330 / 문제: 두 수 비교하기 / 완료

---
+++ a처음부터_다시푸는_백준/브론즈/조건문/두수비교하기_1330/Main.java
@@ -0,0 +1,24 @@
+package a처음부터_다시푸는_백준.브론즈.조건문.두수비교하기_1330;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a > b) {
+            System.out.println(">");
+        } else if (a == b) {
+            System.out.println("==");
+        } else if (a < b) {
+            System.out.println("<");
+        }
+    }
+}
