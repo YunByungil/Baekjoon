2ff51ec39f9bec4ad85feda1d7460514e9700d0c
YunByungil <dbsquddlfz@naver.com>
2023-04-14 21:04:41 +0900

    프로그래머스: SQL / 문제: 강원도에 위치한 생산공장 목록 출력하기 / 완료

---
+++ 프로그래머스/sql/Level1/강원도에_위치한_생산공장_목록_출력하기/Main.java
@@ -0,0 +1,10 @@
+package 프로그래머스.sql.Level1.강원도에_위치한_생산공장_목록_출력하기;
+
+public class Main {
+    /*
+    SELECT factory_id, factory_name, address
+    from food_factory
+    where address like '%강원도%'
+    order by factory_id
+     */
+}
