1e76339968559ee4be0f6f8b5834fe9bca63c3ca
YunByungil <dbsquddlfz@naver.com>
2023-04-15 23:45:28 +0900

    프로그래머스: SQL / 문제: 어린 동물 찾기 / 완료

---
+++ 프로그래머스/sql/Level1/어린_동물_찾기/Main.java
@@ -0,0 +1,10 @@
+package 프로그래머스.sql.Level1.어린_동물_찾기;
+
+public class Main {
+   /*
+   SELECT animal_id, name
+   from animal_ins
+   where intake_condition != 'aged'
+   order by animal_id
+    */
+}
