f7b54b57fd8752a42c84ed77cce0d49f08a9ced2
YunByungil <dbsquddlfz@naver.com>
2023-04-23 19:59:08 +0900

    프로그래머스: SQL / 문제: 아픈 동물 찾기 / 완료

---
+++ 프로그래머스/sql/Level1/아픈_동물_찾기/Main.java
@@ -0,0 +1,10 @@
+package 프로그래머스.sql.Level1.아픈_동물_찾기;
+
+public class Main {
+    /*
+    SELECT animal_id, name
+    from animal_ins
+    where INTAKE_CONDITION = 'sick'
+    order by animal_id;
+     */
+}
