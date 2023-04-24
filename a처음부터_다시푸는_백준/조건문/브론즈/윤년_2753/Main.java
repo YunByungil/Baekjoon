91b6c4a679551a124cb68662618a0ccb4e718ae3
YunByungil <dbsquddlfz@naver.com>
2023-04-24 12:24:31 +0900

    백준: no2753 / 문제: 윤년 / 완료

---
+++ a처음부터_다시푸는_백준/조건문/브론즈/윤년_2753/Main.java
@@ -0,0 +1,18 @@
+package a처음부터_다시푸는_백준.조건문.브론즈.윤년_2753;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
