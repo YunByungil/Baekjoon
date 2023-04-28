330e2317433e32fb7b9c7d26421a5046a954e078
YunByungil <dbsquddlfz@naver.com>
2023-04-28 14:05:08 +0900

    바킹독: 스택 / 기본 문제: 제로(10773) / 완료

---
+++ _바킹독_알고리즘/스택/제로/Main.java
@@ -0,0 +1,30 @@
+package _바킹독_알고리즘.스택.제로;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int k;
+    public static Stack<Integer> stack = new Stack<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result = 0;
+        k = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < k; i++) {
+            int input = Integer.parseInt(br.readLine());
+
+            if (input == 0) {
+                stack.pop();
+            } else {
+                stack.push(input);
+            }
+        }
+
+        for (Integer money : stack) {
+            result += money;
+        }
+
+        System.out.println(result);
+    }
+}
