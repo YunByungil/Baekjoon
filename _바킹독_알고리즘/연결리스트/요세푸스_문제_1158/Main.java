37a2a8a1ccc6c1ba4827da829fa2b62f16b7cf46
YunByungil <dbsquddlfz@naver.com>
2023-04-28 13:30:25 +0900

    바킹독: 연결리스트 / 기본 문제: 요세푸스 문제(1158) / 복습 완료

--- _바킹독_알고리즘/연결리스트/요세푸스_문제_1158/Main.java
+++ _바킹독_알고리즘/연결리스트/요세푸스_문제_1158/Main.java
@@ -4,23 +4,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     public static int n, k;
     public static LinkedList<Integer> list = new LinkedList<>();
+    public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        list.add(1);
-        list.add(2);
-        list.add(3);
 
-        System.out.println(list.poll());
+        for (int i = 1; i <= n; i++) {
+            list.offer(i);
+        }
+
+        sb.append("<");
+
+        while (list.size() > 1) {
+            for (int i = 0; i < k - 1; i++) {
+                list.offer(list.poll());
+            }
+
+            sb.append(list.poll()).append(", ");
+        }
+
+        sb.append(list.poll()).append(">");
+
+        System.out.println(sb);
     }
 }
