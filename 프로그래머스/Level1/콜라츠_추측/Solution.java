e278f4250ce94a9d4480e06defa091f6a523f1ee
YunByungil <dbsquddlfz@naver.com>
2023-04-28 13:24:31 +0900

    프로그래머스: Level1 / 문제: 콜라츠 추측 / 완료

---
+++ 프로그래머스/Level1/콜라츠_추측/Solution.java
@@ -0,0 +1,30 @@
+package 프로그래머스.Level1.콜라츠_추측;
+
+public class Solution {
+    public static int solution (int num){
+        int answer = 0;
+        if(num != 1){
+            while(true) {
+                answer++;
+                if (num % 2 == 0) {
+                    num = num / 2;
+                }
+                else if (num % 2 == 1) {
+                    num = num * 3 + 1;
+                }
+                if (num == 1) {
+                    break;
+                }
+                if (answer>=500){
+                    answer =-1;
+                    break;
+                }
+            }
+            return answer;
+        }
+        else{
+            answer = 0;
+            return answer;
+        }
+    }
+}
