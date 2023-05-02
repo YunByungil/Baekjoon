3d3651a928fa2e47cc15b4ffa08032020bb3bca4
YunByungil <dbsquddlfz@naver.com>
2023-05-02 14:19:16 +0900

    프로그래머스: Level0 / 문제: 수열과 구간 쿼리4 / 완료

---
+++ 프로그래머스/Level0/수열과_구간_쿼리4/Solution.java
@@ -0,0 +1,21 @@
+package 프로그래머스.Level0.수열과_구간_쿼리4;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        // query [s, e, k]
+        // s <= i <= e     i에 대해 i가 k의 배수이면 arr[i]에 1을 더합니다.
+        for (int i = 0; i < queries.length; i++) {
+            int a = queries[i][0];
+            int b = queries[i][1];
+            for (int j = a; j <= b; j++) {
+                if (j % queries[i][2] == 0) {
+                    arr[j]++;
+                }
+            }
+        }
+
+        answer = arr;
+        return answer;
+    }
+}
