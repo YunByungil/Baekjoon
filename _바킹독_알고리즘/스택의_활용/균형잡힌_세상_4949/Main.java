849bb280ad3d00997c9251152ef6a0379e11e172
YunByungil <dbsquddlfz@naver.com>
2023-05-03 11:44:08 +0900

    바킹독: 스택의 활용 / 연습 문제: 균형잡힌 세상(4949) / 완료

---
+++ _바킹독_알고리즘/스택의_활용/균형잡힌_세상_4949/Main.java
@@ -0,0 +1,53 @@
+package _바킹독_알고리즘.스택의_활용.균형잡힌_세상_4949;
+
+import java.util.*;
+import java.io.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = "";
+        while (true) {
+            Stack<Character> stack = new Stack<>();
+            str = br.readLine();
+            if (str.equals(".")) {
+                break;
+            }
+
+            for (int i = 0; i < str.length(); i++) {
+                char input = str.charAt(i);
+                if (input == '(' || input == '[') {
+                    stack.push(input);
+                } else if (input == ')') {
+                    if (!stack.isEmpty()) {
+                        if (stack.peek() == '(') {
+                            stack.pop();
+                        } else {
+                            stack.push(input);
+                        }
+                    } else {
+                        stack.push(input);
+                    }
+                } else if (input == ']') {
+                    if (!stack.isEmpty()) {
+                        if (stack.peek() == '[') {
+                            stack.pop();
+                        } else {
+                            stack.push(input);
+                        }
+                    } else {
+                        stack.push(input);
+                    }
+                }
+            }
+
+            if (stack.size() == 0) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+
+
+        }
+    }
+}
