c96ad0ebe4839deb28f8ab4e7ca3c259d61e857e
YunByungil <dbsquddlfz@naver.com>
2023-05-25 18:17:35 +0900

    바킹독: 시뮬레이션 / 기본 문제: 트럭(13335) / 풀이가 생각났음, 다시 풀기

---
+++ _바킹독_알고리즘/시뮬레이션/트럭_13335/Main.java
@@ -0,0 +1,47 @@
+package _바킹독_알고리즘.시뮬레이션.트럭_13335;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n, w, l; // n: 다리를 건너는 트럭 수, w: 다리 길이, l: 다리 최대 하중
+    public static Queue<Integer> bridge = new LinkedList<>();
+    public static Queue<Integer> truck = new LinkedList<>();
+    public static int now, time;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        w = Integer.parseInt(st.nextToken());
+        l = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int truckWeight = Integer.parseInt(st.nextToken());
+            truck.offer(truckWeight);
+        }
+
+        for (int i = 0; i < w; i++) {
+            bridge.offer(0);
+        }
+
+        while (!bridge.isEmpty()) {
+            time++;
+            int bridgeWeight = bridge.poll();
+            now -= bridgeWeight;
+
+            if (!truck.isEmpty()) {
+                if (now + truck.peek() <= l) {
+                    int truckWeight = truck.poll();
+                    bridge.offer(truckWeight);
+                    now += truckWeight;
+                } else {
+                    bridge.offer(0);
+                }
+            }
+        }
+
+        System.out.println(time);
+    }
+}
