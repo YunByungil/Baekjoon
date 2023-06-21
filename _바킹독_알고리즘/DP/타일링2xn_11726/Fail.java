d950a4e9b61daaa2ee62e084cad0a39a5d845ca3
YunByungil <dbsquddlfz@naver.com>
2023-06-21 09:08:16 +0900

    바킹독: DP / 연습 문제: 2xn 타일링(11726) / 접근 못함

---
+++ _바킹독_알고리즘/DP/타일링2xn_11726/Fail.java
@@ -0,0 +1,24 @@
+package _바킹독_알고리즘.DP.타일링2xn_11726;
+
+import java.util.*;
+import java.io.*;
+
+public class Fail {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+
+    }
+}
