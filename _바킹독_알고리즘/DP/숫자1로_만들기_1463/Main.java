c9d471549ad34950c87a3b06665697d549332c56
YunByungil <dbsquddlfz@naver.com>
2023-06-27 12:58:41 +0900

    바킹독: DP / 연습 문제: 1로 만들기(1463) / 다시 풀기 완료

---
+++ _바킹독_알고리즘/DP/숫자1로_만들기_1463/Main.java
@@ -0,0 +1,29 @@
+package _바킹독_알고리즘.DP.숫자1로_만들기_1463;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        // dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
+            }
+
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+            }
+        }
+
+        System.out.println(dp[10]);
+
+    }
+}
