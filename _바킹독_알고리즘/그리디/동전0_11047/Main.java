4368f9f8098f6b93140618d24e4f99b6fcfdc51d
YunByungil <dbsquddlfz@naver.com>
2023-07-03 07:05:07 +0900

    바킹독: 그리디 / 연습 문제: 동전0(11047) / 복습 완료

---
+++ _바킹독_알고리즘/그리디/동전0_11047/Main.java
@@ -0,0 +1,29 @@
+package _바킹독_알고리즘.그리디.동전0_11047;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int answer = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            answer += k / arr[i];
+            k = k % arr[i];
+        }
+
+        System.out.println(answer);
+
+    }
+}
+
