0e62fe61db1c61abcf2443496576811625d596ac
YunByungil <dbsquddlfz@naver.com>
2023-07-06 16:23:15 +0900

    바킹독: 그리디 / 기본 문제: ATM(11399) / 완료

---
+++ _바킹독_알고리즘/그리디/ATM_11399/Main.java
@@ -0,0 +1,32 @@
+package _바킹독_알고리즘.그리디.ATM_11399;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int result = arr[0];
+        int answer = result;
+        for (int i = 1; i < n; i++) {
+            result += arr[i];
+            answer += result;
+        }
+
+        System.out.println(answer);
+
+    }
+}
+
