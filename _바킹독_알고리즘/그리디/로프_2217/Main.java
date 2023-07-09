fa93a848781687942ce0ec11cbfbccc756cae049
YunByungil <dbsquddlfz@naver.com>
2023-07-09 14:34:42 +0900

    바킹독: 그리디 / 연습 문제: 로프(2217) / 복습

---
+++ _바킹독_알고리즘/그리디/로프_2217/Main.java
@@ -0,0 +1,28 @@
+package _바킹독_알고리즘.그리디.로프_2217;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            max = Math.max(max, i * arr[n - i]);
+        }
+
+        System.out.println(max);
+    }
+}
+
