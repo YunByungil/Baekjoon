9ebc372b727abc98dc6c4d1dfb1e94d9c15479f1
YunByungil <dbsquddlfz@naver.com>
2023-07-13 17:49:52 +0900

    백준: 수학 / 연습 문제: 카잉 달력(6064) / 실패

---
+++ _바킹독_알고리즘/수학/카잉_달력_6064/Fail.java
@@ -0,0 +1,34 @@
+package _바킹독_알고리즘.수학.카잉_달력_6064;
+
+import java.util.*;
+import java.io.*;
+
+public class Fail {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int t = Integer.parseInt(br.readLine());
+        for (int count = 0; count < t; count ++) {
+            st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+
+            boolean check = false;
+
+            for (int i = x; i < (m * n); i += m) {
+                if (i % n == y) {
+                    check = true;
+                    System.out.println(i + 1);
+                    break;
+                }
+            }
+
+            if (!check) {
+                System.out.println(-1);
+            }
+        }
+    }
+}
