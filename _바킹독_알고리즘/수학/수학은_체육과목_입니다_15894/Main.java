993c728087e6749f1505d1863f105aa7ed3d0b74
YunByungil <dbsquddlfz@naver.com>
2023-07-15 13:35:57 +0900

    바킹독: 수학 / 기본 문제: 수학은 체육과목 입니다(15894) / 완료

---
+++ _바킹독_알고리즘/수학/수학은_체육과목_입니다_15894/Main.java
@@ -0,0 +1,16 @@
+package _바킹독_알고리즘.수학.수학은_체육과목_입니다_15894;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        long n = Long.parseLong(br.readLine());
+        long answer = n * 4;
+        System.out.println(answer);
+
+    }
+}
