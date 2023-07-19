5fab22e0fe5b2c0bd2071dff333781e78483c8f3
YunByungil <dbsquddlfz@naver.com>
2023-07-19 18:27:27 +0900

    바킹독: 이분탐색 / 연습 문제: 수 찾기(1920) / 완료

---
+++ _바킹독_알고리즘/이분탐색/수_찾기_1920/Main.java
@@ -0,0 +1,53 @@
+package _바킹독_알고리즘.이분탐색.수_찾기_1920;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n, m;
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            binarySearch(num);
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void binarySearch(int num) {
+        int start = 0;
+        int end = arr.length;
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] > num) {
+                end = mid;
+            } else if (arr[mid] < num) {
+                start = mid + 1;
+            } else {
+                sb.append(1).append("\n");
+                return;
+            }
+        }
+
+        sb.append(0).append("\n");
+    }
+}
+
