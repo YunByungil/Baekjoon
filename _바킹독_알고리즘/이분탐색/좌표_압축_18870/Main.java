243b6769242ccbb44f14bc8f58b12100132dd04f
YunByungil <dbsquddlfz@naver.com>
2023-07-20 17:55:54 +0900

    바킹독: 이분탐색 / 연습 문제: 좌표 압축(18870) / 틀렸음

---
+++ _바킹독_알고리즘/이분탐색/좌표_압축_18870/Main.java
@@ -0,0 +1,41 @@
+package _바킹독_알고리즘.이분탐색.좌표_압축_18870;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] sorted = new int[n];
+        int rank = 0;
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sorted[i] = arr[i];
+        }
+
+        Arrays.sort(sorted);
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i : sorted) {
+            if (!map.containsKey(i)) {
+                map.put(i, rank);
+                rank++;
+            }
+        }
+
+        for (int i : arr) {
+            sb.append(map.get(i)).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
