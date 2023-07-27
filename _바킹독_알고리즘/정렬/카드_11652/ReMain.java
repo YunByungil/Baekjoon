e07e12a543a71a75421998c9f69a48597a72c706
YunByungil <dbsquddlfz@naver.com>
2023-07-27 23:51:48 +0900

    바킹독: 정렬 / 연습 문제: 카드(11652) / 다시 풀기 완료

---
+++ _바킹독_알고리즘/정렬/카드_11652/ReMain.java
@@ -0,0 +1,40 @@
+package _바킹독_알고리즘.정렬.카드_11652;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class ReMain {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int maxCount = 0;
+        long maxValue = Long.MIN_VALUE;
+        int count = 0;
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || arr[i - 1] == arr[i]) {
+                count++;
+            } else {
+                if (count > maxCount) {
+                    maxValue = arr[i - 1];
+                    maxCount = count;
+                }
+
+                count = 1;
+            }
+        }
+
+        if (count > maxCount) {
+            maxValue = arr[n - 1];
+        }
+
+        System.out.println(maxValue);
+    }
+}
