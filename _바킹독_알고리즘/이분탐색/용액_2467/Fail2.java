d531f60c40c64cb6f39933e497a8645a2c5547cf
YunByungil <dbsquddlfz@naver.com>
2023-08-05 14:16:12 +0900

    바킹독: 이분탐색 / 기본 문제: 용액(2467) / 틀렸음 - 투 포인터, 이분탐색

---
+++ _바킹독_알고리즘/이분탐색/용액_2467/Fail2.java
@@ -0,0 +1,64 @@
+package _바킹독_알고리즘.이분탐색.용액_2467;
+
+import java.util.*;
+import java.io.*;
+
+public class Fail2 {
+    public static int n, max;
+    public static int[] arr;
+    public static int[] sorted;
+    public static int[] answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        n = Integer.parseInt(br.readLine());
+        max = Integer.MAX_VALUE;
+        arr = new int[n];
+        sorted = new int[n];
+        answer = new int[2];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sorted[i] = arr[i];
+        }
+        Arrays.sort(sorted);
+
+        // int start = 0;
+        // int end = n - 1;
+
+        int resultS = 0;
+        int resultE = 0;
+        int nowTem = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            int start = i + 1;
+            int end = n;
+
+            while (start < end) {
+                int mid = (start + end) / 2;
+
+                int result = sorted[i] + sorted[mid];
+                int check = Math.abs(result);
+
+                if (max >= check) {
+                    max = check;
+                    resultS = i;
+                    resultE = mid;
+                }
+
+                if (result == 0) {
+                    break;
+                } else if (result > 0) {
+                    end = mid;
+                } else if (result < 0) {
+                    start = mid + 1;
+                }
+            }
+        }
+
+        System.out.println(sorted[resultS] + " " + sorted[resultE]);
+    }
+}
+
