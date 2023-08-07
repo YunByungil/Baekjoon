3449f843535783d8a5702e9354dfafe95c9e9136
YunByungil <dbsquddlfz@naver.com>
2023-08-07 19:33:59 +0900

    바킹독: 투 포인터 / 연습 문제: 수 고르기(2230) / 틀렸음

---
+++ _바킹독_알고리즘/투포인터/수_고르기_2230/Fail.java
@@ -0,0 +1,44 @@
+package _바킹독_알고리즘.투포인터.수_고르기_2230;
+
+import java.util.*;
+import java.io.*;
+
+public class Fail {
+    public static int n, m;
+    public static long answer = 0;
+    public static int[] arr, sorted;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        // int start = 0;
+        int end = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int start = 0; start < n; start++) {
+            while (end < n && arr[end] - arr[start] < m) {
+                end++;
+            }
+
+            if (end == n) {
+                break;
+            }
+
+            min = Math.min(min, arr[end] - arr[start]);
+        }
+
+
+        System.out.println(min);
+    }
+}
