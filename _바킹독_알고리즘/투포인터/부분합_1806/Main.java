e6ea483b34faed1b2033b7629b432c1e19fff87e
YunByungil <dbsquddlfz@naver.com>
2023-08-09 16:14:00 +0900

    바킹독: 투 포인터 / 연습 문제: 부분합(1806) / 복습

---
+++ _바킹독_알고리즘/투포인터/부분합_1806/Main.java
@@ -0,0 +1,48 @@
+package _바킹독_알고리즘.투포인터.부분합_1806;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        // Arrays.sort(arr);
+
+        int answer = Integer.MAX_VALUE;
+        int end = 0;
+        int num = arr[0];
+
+        for (int start = 0; start < n; start++) {
+            while (end < n && num < s) {
+                end++;
+                if (end != n) {
+                    num += arr[end];
+                }
+            }
+
+            if (end == n) {
+                break;
+            }
+
+            answer = Math.min(answer, (end - start) + 1);
+            num -= arr[start];
+        }
+
+        if (answer == Integer.MAX_VALUE) {
+            answer = 0;
+        }
+        System.out.println(answer);
+    }
+}
