2c71f8d46cde9e10a0066053b73e44014f0bf375
YunByungil <dbsquddlfz@naver.com>
2023-08-13 12:00:20 +0900

    바킹독: 해시 / 연습 문제: 나는야 포켓몬 마스터 이다솜(1620) / 완료

---
+++ _바킹독_알고리즘/해시/나는야_포켓몬_마스터_이다솜_1620/Main.java
@@ -0,0 +1,48 @@
+package _바킹독_알고리즘.해시.나는야_포켓몬_마스터_이다솜_1620;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        String[] arr = new String[n + 1];
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            String name = br.readLine();
+            map.put(name, i);
+            arr[i] = name;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            String input = br.readLine();
+            if (isNum(input)) {
+                sb.append(arr[Integer.parseInt(input)]).append("\n");
+            } else {
+                sb.append(map.get(input)).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    public static boolean isNum(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
