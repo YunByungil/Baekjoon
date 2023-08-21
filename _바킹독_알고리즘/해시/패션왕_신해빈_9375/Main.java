ff0efa12a3cd1dd3154b0c67470d7f91774d5d7c
YunByungil <dbsquddlfz@naver.com>
2023-08-21 16:11:21 +0900

    바킹독: 해시 / 기본 문제: 패션왕 신해빈(9375) / 복습

---
+++ _바킹독_알고리즘/해시/패션왕_신해빈_9375/Main.java
@@ -0,0 +1,33 @@
+package _바킹독_알고리즘.해시.패션왕_신해빈_9375;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int t = Integer.parseInt(br.readLine()); // 테스트 케이스
+
+        for (int count = 0; count < t; count++) {
+            int n = Integer.parseInt(br.readLine()); // 의상의 수 n
+            Map<String, Integer> map = new HashMap<>();
+
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                String name = st.nextToken();
+                String type = st.nextToken();
+
+                map.put(type, map.getOrDefault(type, 0) + 1);
+            }
+
+            int answer = 1;
+            for (Integer value : map.values()) {
+                answer *= (value + 1);
+            }
+            System.out.println(answer - 1);
+        }
+    }
+}
